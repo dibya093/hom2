de99266c0b53f3dd5814dd33a2a9cb656c8c2156
HAPPY <HAPPY@LAPTOP-LIP92R15>
2022-03-24 22:34:10 +0530

    my first commit

---
+++ com.demo.practice2/src/test/java/com/demo/practice2/CreateProduct1Test.java
@@ -0,0 +1,11 @@
+package com.demo.practice2;
+
+import org.testng.annotations.Test;
+
+public class CreateProduct1Test {
+	@Test
+	public void CreateProduct1() {
+		System.out.println("user2 rohit");
+	}
+
+}
